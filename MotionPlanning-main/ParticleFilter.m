3d1d2d1a94967639ed5f0d30a50ec6b50e6d9848
bakisanlan <b.sanlan.1325@gmail.com>
2023-11-09 00:04:39 +0300

    Particle Filter Class is improved
    -Particle Filter Class is improved
    -PF_main with class is created

--- MotionPlanning-main/ParticleFilter.m
+++ MotionPlanning-main/ParticleFilter.m
@@ -5,31 +5,28 @@ classdef ParticleFilter < handle
         x_range
         y_range
         hdg_range
-        std
+        alt_std
+        imu_std
         dt
         particles
         weights
         particles_elevation
-        dted
         alt
-        mean
-        var
-
     end
 %% PF Functions
     methods
-        function self = ParticleFilter(N,x_range,y_range,hdg_range,std,dt,dted,alt)
+        function self = ParticleFilter(N,x_range,y_range,hdg_range,alt_std,imu_std,dt,alt)
             self.N = N;
             self.x_range = x_range;
             self.y_range = y_range;
             self.hdg_range = hdg_range;
-            self.std = std;
+            self.alt_std = alt_std;
+            self.imu_std = imu_std;
             self.dt = dt;
             self.weights = ones(self.N,1)/self.N;
-            self.dted = dted;
             self.alt = alt;
 
-            self.create_uniform_particles(self);
+            self.create_uniform_particles;
         end
         
         function aircraft_pos = aircraft_step(self,aircraft_pos, u)
@@ -39,11 +36,11 @@ classdef ParticleFilter < handle
             % kinematik model of UAV
             
             % Update heading
-            aircraft_pos(3) = u(1) + (randn(1) * self.std(1));
+            aircraft_pos(3) = u(1) + (randn(1) * self.imu_std(1));
             aircraft_pos(3) = mod(aircraft_pos(3), 2*pi);
             
             % Move in the (noisy) commanded direction
-            velocity = (u(2) * self.dt) + (randn(1) * self.std(2));
+            velocity = (u(2) * self.dt) + (randn(1) * self.imu_std(2));
             aircraft_pos(1) = aircraft_pos(1) + cos(aircraft_pos(3)) .* velocity;
             aircraft_pos(2) = aircraft_pos(2) + sin(aircraft_pos(3)) .* velocity;
         end
@@ -56,37 +53,37 @@ classdef ParticleFilter < handle
             
             
             % Update heading
-            self.particles(:, 3) = u(1) + (randn(self.N, 1) .* self.std(1));
+            self.particles(:, 3) = u(1) + (randn(self.N, 1) .* self.imu_std(1));
             self.particles(:, 3) = mod(self.particles(:, 3), 2*pi);
             
             % Move in the (noisy) commanded direction
-            velocity = (u(2) * self.dt) + (randn(self.N, 1) * self.std(2));
+            velocity = (u(2) * self.dt) + (randn(self.N, 1) * self.imu_std(2));
             self.particles(:, 1) = self.particles(:, 1) + cos(self.particles(:, 3)) .* velocity;
             self.particles(:, 2) = self.particles(:, 2) + sin(self.particles(:, 3)) .* velocity;
         end
         
-        function update_weights(self, radar_data)
+        function update_weights(self, radar_data,dted)
             % update weight based on PDF value
 
             % updating elevation data of particles
-            self.find_elev_particles(self);
+            self.find_elev_particles(dted);
 
-            self.weights = self.weights .* normpdf(radar_data,self.particles_elevation,self.std);
+            self.weights = self.weights .* normpdf(radar_data,self.particles_elevation,self.alt_std);
             self.weights = self.weights + 1e-300;
             self.weights = self.weights ./ sum(self.weights);
 
-            if self.neff(self) < self.N/2
-                indexes = self.resample_Systematic(self);
-                self.resample(self,indexes)
+            if self.neff < self.N/2
+                indexes = self.resample_Systematic;
+                self.resample(indexes)
             end
         end
         
-        function estimate(self)
+        function [mean, var] = estimate(self)
             % taking weighted mean and var of particles for estimation 
             %%returns mean and variance of the weighted particles
             pos = self.particles(:, 1:2);
-            self.mean = sum(pos .* self.weights) / sum(self.weights);
-            self.var  = sum((pos - self.mean).^2 .* self.weights) / sum(self.weights);
+            mean = sum(pos .* self.weights) / sum(self.weights);
+            var  = sum((pos - mean).^2 .* self.weights) / sum(self.weights);
         end
     
     end
@@ -105,11 +102,11 @@ classdef ParticleFilter < handle
             self.particles(:, 3) = mod(self.particles(:, 3),2*pi);
         end
 
-        function find_elev_particles(self)
+        function find_elev_particles(self,dted)
             % find elevation of particles from DTED
     
-            self.particles_elevation = interp2(self.dted{1},self.dted{2},self.dted{3},self.particles(:,1),self.particles(:,2));
-            self.particles_elevation = self.alt - self.particles_elevation + randn(self.N,1)*self.std;
+            self.particles_elevation = interp2(dted{1},dted{2},dted{3},self.particles(:,1),self.particles(:,2));
+            self.particles_elevation = self.alt - self.particles_elevation + randn(self.N,1)*self.alt_std;
         end
 
         function indx = resample_Systematic(self)
